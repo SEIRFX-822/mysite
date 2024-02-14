d955e03c4cace5917517c2ba306194539ea32cc2
Rome Bell <romebell5@gmail.com>
2024-02-13 21:08:26 -0800

    feat: complete login functionality

--- polls/urls.py
+++ polls/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path("polls/<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # ex: /polls/5/vote
     path("polls/<int:question_id>/vote/", views.vote, name="vote"),
-    
+    path('login/', views.login_view, name="login")
 ]
